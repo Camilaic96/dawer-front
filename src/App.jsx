bb5a1b043bab483d4caa90abf85a8207a9894d17
camila cuello <camilaicuello@hotmail.com>
2023-08-19 14:40:05 -0300

    Page structure

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer.jsx';
+import Header from './components/Header.jsx';
+import Cart from './pages/Cart.jsx';
+import Checkout from './pages/Checkout.jsx';
+import Error404 from './pages/Error404.jsx';
+import ForgotPassword from './pages/ForgotPassword.jsx';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import PanelAdmin from './pages/PanelAdmin.jsx';
+import Products from './pages/Products.jsx';
+import Profile from './pages/Profile.jsx';
+import Register from './pages/Register.jsx';
+
+function App() {
+	return (
+		<div>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path={'/'} element={<Home />} />
+					<Route path={'/productos/'} element={<Products />} />
+					<Route path={'/productos/:catId'} element={<Products />} />
+					<Route path={'/carrito'} element={<Cart />} />
+					<Route path={'/ticket'} element={<Checkout />} />
+					<Route path={'/login'} element={<Login />} />
+					<Route path={'/registro'} element={<Register />} />
+					<Route path={'/perfil'} element={<Profile />} />
+					<Route path={'/password'} element={<ForgotPassword />} />
+					<Route path={'/panel'} element={<PanelAdmin />} />
+					<Route path={'*'} element={<Error404 />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</div>
+	);
+}
+
+export default App;
