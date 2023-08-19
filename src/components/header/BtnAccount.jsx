6c4b5e1fe5fdab754733f41f43e75b93f1a1a2c5
camila cuello <camilaicuello@hotmail.com>
2023-08-19 18:00:01 -0300

    views: header

---
+++ src/components/header/BtnAccount.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+
+const btnAccount = () => {
+	const userRole = 'ADMIN';
+	// const userRole = 'USER';
+	return (
+		<div className="dropdown" id="btn-account-nav">
+			<button
+				className="dropdown-toggle border-0 d-flex align-items-center"
+				type="button"
+				data-bs-toggle="dropdown"
+				aria-expanded="false"
+			>
+				<img src={'../img/icons/account.png'} width={36} alt="botón cuenta" />
+			</button>
+			<ul className="dropdown-menu dropdown-menu-end">
+				<li className="mb-3">
+					<Link to={'/perfil'} className="li-btn-account">
+						Perfil
+					</Link>
+				</li>
+				{userRole === 'ADMIN' && (
+					<li className=" mb-3">
+						<Link to={'/panel'} className="li-btn-account">
+							Panel
+						</Link>
+					</li>
+				)}
+				<li>
+					<button className="li-btn-account">Cerrar Sesión</button>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default btnAccount;
---
+++ src/components/header/BtnCart.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const BtnCart = () => {
+	return (
+		<Link to={'/carrito'} id="btn-cart-nav">
+			<img src={'../img/icons/cart-red.png'} alt="carrito" />
+			<span className="badge rounded-pill">0 {/* {cartTotal()} */}</span>
+		</Link>
+	);
+};
+
+export default BtnCart;
