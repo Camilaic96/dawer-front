af5440469cb91674d279e6880ff7968804e50045
camila cuello <camilaicuello@hotmail.com>
2023-08-19 18:49:31 -0300

    views: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,62 @@
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer id="footer" className="blue-background">
+			<h2>Contacto</h2>
+			<p>
+				Podés conocernos y realizar tu consulta a través de nuestras redes.
+				¡Estamos para ayudarte!
+			</p>
+			<div className="container-redes">
+				<div className="col-6 col-md-3">
+					<img
+						src={'../img/icons/email-blue.png'}
+						alt="email"
+						className="redes"
+					/>
+					<p>info@dawersabores.com.ar</p>
+				</div>
+				<a
+					href="https://www.facebook.com/dawersabores"
+					rel="noreferrer"
+					target="_blank"
+					className="col-6 col-lg-3"
+				>
+					<img
+						src={'../img/icons/facebook-blue.png'}
+						alt="Ir a Facebook"
+						className="redes"
+					/>
+					<p>Dawer.sabores</p>
+				</a>
+				<a
+					href="https://wa.me/+5491154998486"
+					rel="noreferrer"
+					target="_blank"
+					className="col-6 col-md-3"
+				>
+					<img
+						src={'../img/icons/whatsapp-blue.png'}
+						alt="Ir a Whatsapp"
+						className="redes"
+					/>
+					<p>+5491154998486</p>
+				</a>
+				<a
+					href="https://www.instagram.com/dawersabores"
+					rel="noreferrer"
+					target="_blank"
+					className="col-6 col-md-3"
+				>
+					<img
+						src={'../img/icons/instagram-blue.png'}
+						alt="Ir a Instagram"
+						className="redes"
+					/>
+					<p>@dawersabores</p>
+				</a>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
