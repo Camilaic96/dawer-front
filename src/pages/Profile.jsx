e34de7b29808f7b466b7b65a3c4d874bc3e24a56
camila cuello <camilaicuello@hotmail.com>
2023-08-19 23:32:32 -0300

    views: profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,30 @@
 const Profile = () => {
-	return <div>Profile</div>;
+	const user = {
+		first_name: '<NAME>',
+		last_name: '<LAST_NAME',
+		age: 26,
+		email: '<EMAIL>',
+	};
+	console.log(user);
+	return (
+		<main id="profile">
+			<h2 className="bold">Mi perfil</h2>
+			<div className="container-profile-info">
+				<p>
+					Nombre: <span className="bold">{user.first_name}</span>
+				</p>
+				<p>
+					Apellido: <span className="bold">{user.last_name}</span>
+				</p>
+				<p>
+					Edad: <span className="bold">{user.age}</span>
+				</p>
+				<p>
+					Correo: <span className="bold">{user.email}</span>
+				</p>
+			</div>
+		</main>
+	);
 };
 
 export default Profile;
