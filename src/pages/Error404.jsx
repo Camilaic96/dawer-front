9ca378f252f9b74b91c5d45b05bba0effb1f580b
camila cuello <camilaicuello@hotmail.com>
2023-08-19 22:03:44 -0300

    views: error404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const Error404 = () => {
-	return <div>Error404</div>;
+	return (
+		<main id="error404">
+			<h2>Error 404</h2>
+			<p>Página no encontrada</p>
+			<Link to={'/'} className="link-404">
+				Volver al inicio
+			</Link>
+		</main>
+	);
 };
 
 export default Error404;
